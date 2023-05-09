ce6642cbcce3fd61731ff6cc8a4b35f7cfbfb2c4
roberto cabral <betocr.dev@gmail.com>
2023-05-08 21:54:49 -0300

    criação de logica de transição

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
 import { HeroPattern } from "./HeroPattern";
 import "./app.css";
+
+import { useState, useTransition } from 'react';
+import ButtonTab from "./components/tabButton";
 import { GetSearch } from "./components/generatorQR";
+import { Encurtador } from "./components/encurtador";
+
 
 function App() {
+  const [isPending, startTransition] = useTransition();
+  const [tab, setTab] = useState('QrCode');
+
+  function selectTab(nextTab) {
+    startTransition(() => {
+      setTab(nextTab);      
+    });
+  }
+
   return (
     <>
       <main className="main">
-        <GetSearch />
+      <div>
+      <ButtonTab
+        isActive={tab === 'QrCode'}
+        onClick={() => selectTab('QrCode')}
+      >
+        gerador de QRcode
+      </ButtonTab>
+
+      <ButtonTab
+        isActive={tab === 'encurtador'}
+        onClick={() => selectTab('encurtador')}
+      >
+        Encurtador
+      </ButtonTab>
+      </div>
+      {tab === 'QrCode' && <GetSearch />}
+      {tab === 'encurtador' && <Encurtador />}
+        
         <HeroPattern />
       </main>
     </>
---
+++ src/components/tabButton/index.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+// eslint-disable-next-line react/prop-types
+export default function ButtonTab({ children, isActive, onClick }) {
+  if (isActive) {
+    return <b>{children}</b>
+  }
+  return (
+    <button onClick={() => {
+      onClick();
+    }}>
+      {children}
+    </button>
+  )
+}
